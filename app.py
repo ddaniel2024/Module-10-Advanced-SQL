b09ec88d88a24b12fd647313b100da28d06f9c5f
ddaniel2024 <daniel97daniel@gmail.com>
2024-04-14 20:15:56 +0100

    working on dynamic API route

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -50,6 +51,7 @@ def precipitation():
 
     start_date = dt.datetime(2017,8,23) - dt.timedelta(days=365)
     rain = session.query(measurement.date, measurement.prcp).filter(measurement.date >= start_date).all()
+    session.close()
 
     precipitation_list = []
     for date, prcp in rain:
@@ -63,17 +65,50 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
 
-    station_activity = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+    stations = session.query(station.station).all()
+    session.close()
 
     station_list = []
-    for station, count in station_activity:
+    for station in stations:
         station_list.append(station)
 
     return(jsonify(station_list))
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return("tobs")
+
+    station_activity = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+    most_active_station_id = station_activity[0][0]
+
+    start_date = dt.datetime(2017,8,23) - dt.timedelta(days=365)
+
+    tobs = session.query(measurement.date, measurement.tobs).filter(measurement.date >= start_date).filter(measurement.station == most_active_station_id).all()
+    session.close()
+
+    tobs_list = []
+    for date, tob in tobs:
+        tobs_dict={}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tob
+        tobs_list.append(tobs_dict)
+
+    return(jsonify(tobs_list))
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+
+    tmin = session.query(measurement.date, func.min(measurement.tobs)).filter(measurement.date >= start).all()
+
+    session.close()
+
+    summary_list = []
+    for date, temp in tmin:
+        summary_dict={}
+        summary_dict["date"] = date
+        summary_dict["temp"] = temp
+        summary_list.append(summary_dict)
+
+    return(jsonify(summary_list))
 
 if __name__ == "__main__":
     app.run(debug=True)
