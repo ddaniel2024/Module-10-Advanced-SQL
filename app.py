61fbf3150c5139e4306112e7007e88867c2f643c
ddaniel2024 <daniel97daniel@gmail.com>
2024-04-14 08:59:25 +0100

    starting flask app

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine(f'sqlite:///hawaii.sqlite')
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
 
@@ -21,10 +25,22 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    print("Welcome to the homepage!")
+    return("ROUTES")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
